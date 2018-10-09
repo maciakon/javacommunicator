d42d5dcb5b561b60d4cc8aa07ec9e617a54f5709
maciakon <k.macialek@gmail.com>
2018-10-09 21:48:36 +0200

    Basic server structure created

---
+++ src/server/Main.java
@@ -0,0 +1,18 @@
+package server;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
+        primaryStage.setTitle("Hello from server");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
