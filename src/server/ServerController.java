b56fbf2f4f5514e46d3bd67cf71758abe82cab7f
maciakon <k.macialek@gmail.com>
2018-10-10 20:48:02 +0200

    Server buttons enabled disabled

--- src/server/ServerController.java
+++ src/server/ServerController.java
@@ -1,6 +1,22 @@
 package server;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ServerController
 {
+    @FXML
+    private Button stopButton, startButton;
 
+    public void startButtonClicked()
+    {
+        startButton.setDisable(true);
+        stopButton.setDisable(false);
+    }
+
+    public void stopButtonClicked()
+    {
+        startButton.setDisable(false);
+        stopButton.setDisable(true);
+    }
 }
