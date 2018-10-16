2bb96e42c117e8c835b2b50ff075d3e8eb8568f5
maciakon <k.macialek@gmail.com>
2018-10-16 22:27:09 +0200

    Runnable client gets anchor pane.

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,6 +1,9 @@
 package client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -8,6 +11,10 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        primaryStage.setTitle("Hello from client");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
