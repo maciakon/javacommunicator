71b5f5b921c9283405cf77af65474568268d39a2
maciakon <k.macialek@gmail.com>
2018-11-21 16:53:59 +0100

    Javadoc created for shared package.

--- src/shared/implementation/PortValidator.java
+++ src/shared/implementation/PortValidator.java
@@ -2,8 +2,16 @@ package shared.implementation;
 
 import shared.interfaces.IPortValidator;
 
+/**
+ * Validates port number parsing a given string.
+ */
 public class PortValidator implements IPortValidator
 {
+    /**
+     * Converts given string into an integer port number.
+     * @param portNumberText string port number
+     * @return integer port number, -1 in case the passed string is not valid.
+     */
     public int GetPortNumberFromString(String portNumberText)
     {
         try
